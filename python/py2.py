12ae21944c7b475208deabe81ff83f49f808ecc9
Faisal <135976229+Fa1sall@users.noreply.github.com>
2023-08-09 22:04:04 -0700

    Add files via upload

---
+++ python/py2.py
@@ -0,0 +1,3 @@
+age=int(input("how old are you?"))
+if 1<age<18:
+    print("You are old enough to vote")
---
+++ python/py5.py
@@ -0,0 +1,9 @@
+a=int(input("Enter a number A:"))
+b=int(input("Enter a number B:"))
+c=int(input("Enter a number C:"))
+if a>b and a>c:
+    print("a is the greatest")
+if b>a and b>c:
+    print("b is the greatest")
+else:
+    print("c is the greatest")
