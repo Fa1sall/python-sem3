29b82c996bd799592e06e6bc69066904842b4c73
Faisal <135976229+Fa1sall@users.noreply.github.com>
2023-08-19 21:34:23 -0700

    Add files via upload

---
+++ data structures/2/2.cpp
@@ -0,0 +1,23 @@
+//Finding consecutive characters from text
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    string text;
+    int i;
+
+    cout<<"Enter a text: "<<endl;
+    cin>>text;
+    cout<<text[0];
+
+    for(i=1; i<text.length() && text[i]!='\0'; i++){
+        if(text[i]==text[i-1]+1){
+            cout<<text[i];
+        }
+        else{
+            cout<<endl<<text[i];
+        }
+    }
+}
