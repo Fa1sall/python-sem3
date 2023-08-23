9adfda3de70b793b72a8945e1fbb84d64435e6cd
Faisal <faisalirfan7c@gmail.com>
2023-08-24 00:04:18 +0530

    changes

---
+++ data structures/3/3.cpp
@@ -0,0 +1,55 @@
+//Inserting and deleting elements from an array
+
+#include<iostream>
+using namespace std;
+
+void Display(int n,int arr[]){
+    cout<<"[";
+    for(int i=0; i<n; i++){
+    cout<<arr[i]<<" ";
+    }
+    cout<<"]"<<endl;
+}
+
+void Insert(int x,int pos,int n,int arr[]){
+    for(int i=n;i>=pos+1;i--){
+        arr[i]=arr[i-1];
+    }
+    arr[pos]=x;
+}
+
+int main(){
+    int i,x,n,size,pos,input;
+    cout<<"Enter size of array:"<<endl;
+    cin>>size;
+    int arr[size];
+    cout<<"Enter number of elements to be inserted:"<<endl;
+    cin>>n;
+    cout<<"Enter the elements: "<<endl;
+    for(i=0;i<n;i++){
+    cin>>arr[i];
+	}
+
+    while(true){
+    cout<<"Enter '1' to Display array: \n"<<"Enter '2' to insert element: \n";
+    cout<<"Enter '3' to remove element: \n"<<"Enter 'E' to Exit: \n";
+    cout<<"Select an option: "<<endl; 
+    cin>>input;
+
+    if(input==1){
+        Display(n,arr);
+    }
+    else if(input==2){
+        n++;
+        cout<<"Enter position of element to be inserted:"<<endl;
+        cin>>pos;
+        cout<<"Enter element to be inserted:"<<endl;
+        cin>>x;
+        Insert(x,pos,n,arr); 
+    }
+    else{
+        break;
+    }
+    }
+ 
+}
