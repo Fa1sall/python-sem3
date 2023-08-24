703b2cbfc16167859355ebea7736fca037c44efe
Faisal <135976229+Fa1sall@users.noreply.github.com>
2023-08-23 21:44:35 -0700

    Add files via upload

---
+++ python/py9.py
@@ -0,0 +1,11 @@
+#Swapping two numbers
+
+x=int(input("Enter number 1: "))
+y=int(input("Enter number 2: "))
+print("Numbers before swapping: ")
+print("n1 =",x," n2 =",y)
+temp=y
+y=x
+x=temp
+print("Numbers after swapping: ")
+print("n1 =",x," n2 =",y)
